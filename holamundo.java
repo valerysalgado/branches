34e69fbc7cc1ae497bd3b22c758b1f775c53de7b
Sadir Cuadro de Horta <scuadrod@gmail.com>
2024-09-26 14:05:25 -0500

    cambio de login

--- holamundo.java
+++ holamundo.java
@@ -1,6 +1,9 @@
 public class holamundo {
 
     public static void main(String[] args) {
+        System.out.println("Nuevo cambio a master desde login");       
+        System.out.println("cambiar");
         
+        System.out.println("cambio nuevo");
     }
 }
